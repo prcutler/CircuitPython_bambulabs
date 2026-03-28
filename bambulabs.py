9f08e1420e7b0a2c45cccc8d897deecf92377b20
Paul Cutler <paul@paulcutler.org>
2026-03-27 20:35:07 -0500

    Initial commit

---
+++ bambulabs.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2026 Paul Cutler
+#
+# SPDX-License-Identifier: MIT
+"""
+`bambulabs`
+================================================================================
+
+A library to interface with a Bambu Labs 3D printer and query status and information
+
+
+* Author(s): Paul Cutler
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+# imports
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/prcutler/CircuitPython_bambulabs.git"
---
+++ docs/examples.rst
@@ -0,0 +1,8 @@
+Simple test
+------------
+
+Ensure your device works with this simple test.
+
+.. literalinclude:: ../examples/bambulabs_simpletest.py
+    :caption: examples/bambulabs_simpletest.py
+    :linenos:
